28422855c90a207ae0ad3322bc94437fdf9340f4
Sean Francis N. Ballais <sean@seanballais.com>
2018-11-24 20:56:05 +0800

    Completed Subject model.

--- api/models.py
+++ api/models.py
@@ -7,23 +7,39 @@ class Base(models.Model):
 
 
 class Section(Base):
-    name = models.TextField("Section Name",
+    name = models.TextField('Name',
                             null=False,
                             blank=False,
                             unique=True)
-    year_level = models.PositiveSmallIntegerField("Year Level",
+    year_level = models.PositiveSmallIntegerField('Year Level',
                                                   null=False,
                                                   blank=False)
 
     def __str__(self):
-        return '{} - {}'.format(name, year_level)
+        return '<{} - {}>'.format(name, year_level)
 
 
 class Batch(Base):
-    year = models.PositiveSmallIntegerField("Year",
+    year = models.PositiveSmallIntegerField('Year',
                                             null=False,
                                             blank=False,
                                             unique=True)
 
     def __str__(self):
-        return 'Batch {}'.format()
+        return '<Batch {}>'.format(year)
+
+
+class Subject(Base):
+    name = models.TextField('Name',
+                            null=False,
+                            blank=False,
+                            unique=True)
+    year_level = models.PositiveSmallIntegerField('Year Level',
+                                                  null=False,
+                                                  blank=False)
+
+    def __str__(self):
+        return '<Subject {} ({})>'.format(name, year_level)
+
+
+
--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.db import IntegrityError
 from api.models import Section
 from api.models import Batch
+from api.models import Subject
 
 
 class SectionModelTestCase(TestCase):
@@ -20,7 +21,7 @@ class SectionModelTestCase(TestCase):
         except Section.DoesNotExist:
             self.fail('Exception occured. '
                       + 'Unable to get a Section object with section name '
-                      + '{}.'.format(self.section_name))
+                      + '"{}".'.format(self.section_name))
         
         self.assertNotEqual(preaddition_count, postaddition_count)
         self.assertEqual(new_section.year_level, 1)
@@ -28,7 +29,7 @@ class SectionModelTestCase(TestCase):
         # any records from the Section table if the section name is different
         # from the section name of the record we want.
 
-    def test_should_not_create_a_section_with_non_unique_name(self):
+    def test_should_not_create_a_section_with_preexisting_name(self):
         Section(name='Section1', year_level=1).save()
         self.assertRaises(IntegrityError,
                           Section(name='Section1', year_level=1).save)
@@ -50,11 +51,42 @@ class BatchModelTestCase(TestCase):
             new_batch = Batch.objects.get(year=self.batch_year)
         except Batch.DoesNotExist:
             self.fail('Exception occured. '
-                      + 'Unable to get a Batch object with year ' 
+                      + 'Unable to get a Batch object of year ' 
                       + '{}.'.format(self.batch_year))
 
         self.assertNotEqual(preaddition_count, postaddition_count)
 
-    def test_should_create_a_batch_with_non_unique_year(self):
+    def test_should_not_create_a_batch_with_preexisting_year(self):
         Batch(year=2018).save()
         self.assertRaises(IntegrityError, Batch(year=2018).save)
+
+
+class SubjectModelTestCase(TestCase):
+    def setUp(self):
+        self.subject_name = 'Subject'
+        self.year_level = 1
+        self.subject = Subject(name=self.subject_name,
+                               year_level=self.year_level)
+
+    def test_can_create_a_subject_properly(self):
+        preaddition_count = Subject.objects.count()
+        self.subject.save()
+
+        postaddition_count = Subject.objects.count()
+
+        try:
+            new_subject = Subject.objects.get(name=self.subject_name)
+        except Subject.DoesNotExist:
+            self.fail('Exception occured. '
+                      + 'Unable to get a Subject object with name '
+                      + '"{}"'.format(self.self.subject_name))
+
+        self.assertNotEqual(preaddition_count, postaddition_count)
+
+    def test_should_not_create_a_subject_with_preexisting_name(self):
+        Subject(name='Subject', year_level=1).save()
+        self.assertRaises(IntegrityError,
+                          Subject(name='Subject', year_level=1).save)
+
+
+
